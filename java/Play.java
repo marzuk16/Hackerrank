0ebcb29718f825adbbaeac134bd9bd399e6e0db6
marzuk <marzuk@eatlbd.com>
2021-11-21 18:55:53 +0600

    ac

---
+++ java/Play.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class Play{
+    public static void main(String[] args) {
+        // String s = "bbfdaaaafd";
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     if(map.get(s.charAt(i)) == null){
+        //         map.put(s.charAt(i), 1);
+        //     }else{
+        //         Integer count = map.get(s.charAt(i)) + 1;
+        //         map.put(s.charAt(i), count);
+        //     }
+        // }
+
+        // for(Character i:map.keySet()){
+        //     System.out.println(i + " " + map.get(i));
+        // }
+
+        String ss = "abc";
+        System.out.println((int)ss.charAt(0));
+        System.out.println((char)9);
+
+
+    }
+}
