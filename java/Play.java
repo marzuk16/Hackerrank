d96f7ba72bf22d8e16d756c701b7433ed9452adb
marzuk <marzuk@eatlbd.com>
2021-11-25 15:21:50 +0600

    s

--- java/Play.java
+++ java/Play.java
@@ -1,22 +1,30 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+import java.util.Set;
+import java.util.LinkedHashSet;
 
-interface InnerPlay {
-    public void sum(int a, int b);
-    public void mul(int a, int b);
-}
+class st {
 
-class In implements InnerPlay {
-    public void sum(int a, int b){
-        System.out.println(a + b);
-    }
-
-    public void mul(int a, int b) {
-        System.out.println(a*b);
-    }
+    public static Integer age;
+    public static String name;    
 }
 public class Play{
     public static void main(String[] args) {
-       In in = new In();
-       in.sum(1,2);
+    
+       st s = new st();
+       s.age = 10;
+       s.name = "Ten";
+       st s1 = new st();
+
+       s1.age = 12;
+       s1.name = "Twelve";
+
+       System.out.println(s.age + " " + s.name);
     }
 }
