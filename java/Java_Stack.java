d9e5331646d48f8041cfc20bff9fa0a68a0a5238
marzuk <marzuk@eatlbd.com>
2021-11-29 15:20:22 +0600

    Stack

---
+++ java/Java_Stack.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Java_Stack{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            //Complete the code
+            int fst = 0, snd = 0, trd = 0;
+            for(int i=0; i<input.length(); i++){
+                if(input.charAt(i) == '(') fst ++;
+                if(input.charAt(i) == ')') fst --;
+                if(input.charAt(i) == '{') snd ++;
+                if(input.charAt(i) == '}') snd --;
+                if(input.charAt(i) == '[') trd ++;
+                if(input.charAt(i) == ']') trd --;
+            }
+            
+            if(fst == 0 && snd == 0 && trd == 0){
+                System.out.println("true");
+            }else {
+                System.out.println("false");
+            }
+            
+		}
+		
+	}
+}
