c158cbb0f912386878b810a34e10f2703d02db48
marzuk <marzuk@eatlbd.com>
2021-12-28 16:44:06 +0600

    delete byte code files

--- java/Play.java
+++ java/Play.java
@@ -8,11 +8,44 @@ import java.util.LinkedHashMap;
 import java.util.TreeMap;
 import java.util.Set;
 import java.util.LinkedHashSet;
-import javafx.util.Pair;
 
+class test{
+    private int a;
+    private String name;
+
+    test(){
+
+    }
+    test(int a, String name){
+        this.a = a;
+        this.name = name;
+    }
+
+    public void run(){
+        System.out.println("parent call");
+    }
+}
+
+class test1 extends test{
+    private int a1;
+    private String name1;
+
+    test1(int a, String name){
+        this.a1 = a;
+        this.name1 = name;
+    }
+
+    public void r(){
+        System.out.println("test1");
+    }
+}
 public class Play{
     public static void main(String[] args) {
-        Pair<Integer, Integer> p = new Pair<Integer, Integer>();
-        System.out.println();
+        System.out.println("Play executing..........");
+
+        test t = new test1(1, "name");
+        // t.r();
+        t.run();
+
     }
 }
