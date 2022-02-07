036bf68fc1c47199a63abb67ab93a542925de5b6
marzuk <marzuk@eatlbd.com>
2022-02-07 16:22:22 +0600

    thread using Thread class

--- java/thread/ExtendsThreadClass.java
+++ java/thread/ExtendsThreadClass.java
@@ -1,11 +1,42 @@
 package thread;
 
-public class ExtendsThreadClass extends Thread {
-    int age;
-    String name;
-    
-    ExtendsThreadClass(){
+class Hi extends Thread{
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Hi");
+            try {
+                Thread.sleep(500);
+                
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.err.println("Hi class error: " + e.getMessage());
+            }
+        }
+    }
+}
+class Hello extends Thread{
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(500);
+                
+            } catch (Exception e) {
+                //TODO: handle exception
+                System.err.println("Hello class error: " + e.getMessage());
+            }
+        }
+    }
+}
+public class ExtendsThreadClass {
 
+    public void threadRunner(){
+
+        Hi hi = new Hi();
+        Hello hello = new Hello();
+    
+        hi.start();
+        hello.start();
     }
     
 }
