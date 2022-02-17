f326f9474a24056247d488e4c7e520183291ce9d
marzuk <marzuk@eatlbd.com>
2022-02-17 17:39:20 +0600

    interface

--- java/interfaces/InterfaceTest.java
+++ java/interfaces/InterfaceTest.java
@@ -1,6 +1,15 @@
 package interfaces;
 
 public interface InterfaceTest {
+
+    default void say(){
+        saySomething();
+    }
+
     int sum(int a, int b);
     int mul(int a, int b);
+
+    private void saySomething(){
+        System.out.println("Private method in interface");
+    }
 }
